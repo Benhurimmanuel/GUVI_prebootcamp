2f21d7323ae16ad6b64fc3f77c28515941fed422
Benhurimmanuel <benhurimmanuel@gmail.com>
2021-02-11 14:36:15 +0530

    Update Circumference.js

--- Circumference.js
+++ Circumference.js
@@ -24,3 +24,31 @@ else
     //end-here
    
 });
+
+
+
+https://docs.google.com/document/d/14J5-zcQjXSNunyUqrXZz50skGX_XxB3IE0b_ilK-Pw4/edit
+
+
+
+
+
+
+
+
+
+
+Write a code to get the input in the given format and print the output in the given format.
+
+Input Description:
+First-line indicates two integers which are the size of array and 'K' value. Second-line indicates an integer contains elements of an array.
+
+Output Description:
+Print the taken input in the same format.
+
+Sample Input :
+5 3
+1 2 3 4 5
+Sample Output :
+5 3
+1 2 3 4 5
